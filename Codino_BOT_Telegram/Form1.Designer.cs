e12b1a9a13a5816fa4d468ef9fd7539c4d713fa7
mrrajabali <mrrajabali@gmail.com>
2025-02-22 13:19:50 +0330

    edit form 1 properties

--- Codino_BOT_Telegram/Form1.Designer.cs
+++ Codino_BOT_Telegram/Form1.Designer.cs
@@ -33,11 +33,14 @@ namespace Codino_BOT_Telegram
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(700, 450);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Codino_BOT_Telegram";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
